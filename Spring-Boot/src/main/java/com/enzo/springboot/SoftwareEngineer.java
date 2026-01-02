9ed7bf164c060d443f171a3e4e0fc3bdeeac8a75
Enzo Sampaio <enzosenzos9@gmail.com>
2026-01-02 00:18:37 -0300

    Get API

---
+++ Spring-Boot/src/main/java/com/enzo/springboot/SoftwareEngineer.java
@@ -0,0 +1,55 @@
+package com.enzo.springboot;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private int id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer(){
+
+    }
+    public SoftwareEngineer(int id, String name, String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return id == that.id && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
